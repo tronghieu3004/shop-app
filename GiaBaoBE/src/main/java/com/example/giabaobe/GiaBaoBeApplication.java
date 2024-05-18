46b33ffdf618632e09dfbb1f183db1fe18f0f63d
tronghieu3004 <letronghieu3004.utm@gmail.com>
2024-05-18 22:23:44 +0700

    spring boot init

---
+++ GiaBaoBE/src/main/java/com/example/giabaobe/GiaBaoBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.giabaobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiaBaoBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiaBaoBeApplication.class, args);
+    }
+
+}
